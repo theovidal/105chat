84565b689a0990fa4b67fe11d85a9b0528b7e44c
Théo Vidal <exybore@becauseofprog.fr>
2020-08-30 22:09:15 +0200

    fix: make CORS work

--- http/middlewares.go
+++ http/middlewares.go
@@ -10,6 +10,13 @@ import (
 // AuthenticationMiddleware checks that user is authenticated before doing a request
 func AuthenticationMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Authentication")
+		if r.Method == http.MethodOptions {
+			Response(w, http.StatusOK, nil)
+			return
+		}
+
 		// Ignoring for this endpoint - We need it to get our access token
 		if r.URL.Path == "/v1/http/auth" {
 			next.ServeHTTP(w, r)
